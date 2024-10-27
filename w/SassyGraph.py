90caa0c55fc112656a2ac895df99c1fa36071d41
Elahe Mohammadi <elahe.mohammadi.elpa@gmail.com>
2024-10-27 10:34:16 -0400

    Update SassyGraph.py
    a lot of features

--- w/SassyGraph.py
+++ w/SassyGraph.py
@@ -17,27 +17,10 @@ print("Original number of rows:", len(data))
 
 # Specify columns to keep
 columns_to_keep = [
-    ' Destination Port', 
-    ' Flow Duration', 
-    ' Total Fwd Packets', 
-    ' Total Backward Packets', 
-    'Total Length of Fwd Packets', 
-    ' Total Length of Bwd Packets', 
-    ' Flow IAT Mean', 
-    ' Fwd Packet Length Mean', 
-    ' Bwd Packet Length Mean', 
-    'Fwd PSH Flags', 
-    ' Bwd PSH Flags', 
-    'FIN Flag Count', 
-    ' SYN Flag Count', 
-    ' ACK Flag Count', 
-    ' Down/Up Ratio', 
-    'Active Mean', 
-    'Idle Mean', 
-    'Fwd Avg Bytes/Bulk', 
-    ' Bwd Avg Bytes/Bulk',
-    ' Label'
+    ' Destination Port', ' Flow Duration', ' Total Fwd Packets', ' Total Backward Packets', 'Total Length of Fwd Packets', ' Total Length of Bwd Packets', ' Fwd Packet Length Max', ' Fwd Packet Length Min', ' Fwd Packet Length Mean', ' Fwd Packet Length Std', 'Bwd Packet Length Max', ' Bwd Packet Length Min', ' Bwd Packet Length Mean', ' Bwd Packet Length Std', ' Flow IAT Mean', ' Flow IAT Std', ' Flow IAT Max', ' Flow IAT Min', 'Fwd IAT Total', ' Fwd IAT Mean', ' Fwd IAT Std', ' Fwd IAT Max', ' Fwd IAT Min', 'Bwd IAT Total', ' Bwd IAT Mean', ' Bwd IAT Std', ' Bwd IAT Max', ' Bwd IAT Min', 'Fwd PSH Flags', ' Bwd PSH Flags', ' Fwd URG Flags', ' Bwd URG Flags', ' Fwd Header Length', ' Bwd Header Length', 'Fwd Packets/s', ' Bwd Packets/s', ' Min Packet Length', ' Max Packet Length', ' Packet Length Mean', ' Packet Length Std', ' Packet Length Variance', 'FIN Flag Count', ' SYN Flag Count', ' RST Flag Count', ' PSH Flag Count', ' ACK Flag Count', ' URG Flag Count', ' CWE Flag Count', ' ECE Flag Count', ' Down/Up Ratio', ' Average Packet Size', ' Avg Fwd Segment Size', ' Avg Bwd Segment Size', ' Fwd Header Length', 'Fwd Avg Bytes/Bulk', ' Fwd Avg Packets/Bulk', ' Fwd Avg Bulk Rate', ' Bwd Avg Bytes/Bulk', ' Bwd Avg Packets/Bulk', 'Bwd Avg Bulk Rate', 'Subflow Fwd Packets', ' Subflow Fwd Bytes', ' Subflow Bwd Packets', ' Subflow Bwd Bytes', 'Init_Win_bytes_forward', ' Init_Win_bytes_backward', ' act_data_pkt_fwd', ' min_seg_size_forward', 'Active Mean', ' Active Std', ' Active Max', ' Active Min', 'Idle Mean', ' Idle Std', ' Idle Max', ' Idle Min', ' Label'
+
 ]
+
 
 '''columns_to_keep = [
     ' Destination Port', ' Flow Duration', ' Total Fwd Packets', 
@@ -88,9 +71,9 @@ class SimpleRNNBinary(nn.Module):
         return torch.sigmoid(out)  # Sigmoid activation for binary classification
 
 # Hyperparameters
-input_size = 19  # Number of features
+input_size = 76  # Number of features
 hidden_size = 50
-num_epochs = 1000
+num_epochs = 100
 learning_rate = 0.001
 
 # Model, loss function, and optimizer
@@ -151,26 +134,10 @@ def analyze_input_file(file_path):
     # Load and process data
     data = pd.read_csv(file_path)
     columns_to_keep = [
-    ' Destination Port', 
-    ' Flow Duration', 
-    ' Total Fwd Packets', 
-    ' Total Backward Packets', 
-    'Total Length of Fwd Packets', 
-    ' Total Length of Bwd Packets', 
-    ' Flow IAT Mean', 
-    ' Fwd Packet Length Mean', 
-    ' Bwd Packet Length Mean', 
-    'Fwd PSH Flags', 
-    ' Bwd PSH Flags', 
-    'FIN Flag Count', 
-    ' SYN Flag Count', 
-    ' ACK Flag Count', 
-    ' Down/Up Ratio', 
-    'Active Mean', 
-    'Idle Mean', 
-    'Fwd Avg Bytes/Bulk', 
-    ' Bwd Avg Bytes/Bulk'
-    ]
+    ' Destination Port', ' Flow Duration', ' Total Fwd Packets', ' Total Backward Packets', 'Total Length of Fwd Packets', ' Total Length of Bwd Packets', ' Fwd Packet Length Max', ' Fwd Packet Length Min', ' Fwd Packet Length Mean', ' Fwd Packet Length Std', 'Bwd Packet Length Max', ' Bwd Packet Length Min', ' Bwd Packet Length Mean', ' Bwd Packet Length Std', ' Flow IAT Mean', ' Flow IAT Std', ' Flow IAT Max', ' Flow IAT Min', 'Fwd IAT Total', ' Fwd IAT Mean', ' Fwd IAT Std', ' Fwd IAT Max', ' Fwd IAT Min', 'Bwd IAT Total', ' Bwd IAT Mean', ' Bwd IAT Std', ' Bwd IAT Max', ' Bwd IAT Min', 'Fwd PSH Flags', ' Bwd PSH Flags', ' Fwd URG Flags', ' Bwd URG Flags', ' Fwd Header Length', ' Bwd Header Length', 'Fwd Packets/s', ' Bwd Packets/s', ' Min Packet Length', ' Max Packet Length', ' Packet Length Mean', ' Packet Length Std', ' Packet Length Variance', 'FIN Flag Count', ' SYN Flag Count', ' RST Flag Count', ' PSH Flag Count', ' ACK Flag Count', ' URG Flag Count', ' CWE Flag Count', ' ECE Flag Count', ' Down/Up Ratio', ' Average Packet Size', ' Avg Fwd Segment Size', ' Avg Bwd Segment Size', ' Fwd Header Length', 'Fwd Avg Bytes/Bulk', ' Fwd Avg Packets/Bulk', ' Fwd Avg Bulk Rate', ' Bwd Avg Bytes/Bulk', ' Bwd Avg Packets/Bulk', 'Bwd Avg Bulk Rate', 'Subflow Fwd Packets', ' Subflow Fwd Bytes', ' Subflow Bwd Packets', ' Subflow Bwd Bytes', 'Init_Win_bytes_forward', ' Init_Win_bytes_backward', ' act_data_pkt_fwd', ' min_seg_size_forward', 'Active Mean', ' Active Std', ' Active Max', ' Active Min', 'Idle Mean', ' Idle Std', ' Idle Max', ' Idle Min'
+
+]
+
 
     filtered_data = data[columns_to_keep]
     scaler = MinMaxScaler(feature_range=(0, 1))
